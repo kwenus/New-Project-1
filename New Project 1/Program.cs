3ff5d9d0058d42f1950c8fde0068a880602244d6
kwenus <kwenus@gmail.com>
2022-01-22 19:28:12 +0300

    first load

---
+++ New Project 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace New_Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = "Константин";
+            Console.WriteLine(name);
+            string surname = "Канатьев";
+            Console.WriteLine(surname);
+        }
+    }
+}
